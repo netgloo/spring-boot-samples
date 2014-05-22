e69de4bc7d2aea11c5656a00f443f8ff8a301182
coderpills <coderpills@gmail.com>
2014-05-22 22:01:37 +0200

    First commit.

---
+++ spring-boot-hot-swapping/src/main/java/hotswapping/controllers/BasicController.java
@@ -0,0 +1,14 @@
+package basewebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BasicController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "hello.html";
+  }
+
+}
