a875605de6b8b06eabb33ab883aef06eef68a640
andreazanelli <andreazanelli5@gmail.com>
2014-05-22 22:15:42 +0200

    Completed this example.

--- spring-boot-basewebapp/src/main/java/basewebapp/controllers/BasicController.java
+++ spring-boot-basewebapp/src/main/java/basewebapp/controllers/BasicController.java
@@ -1,12 +1,14 @@
 package basewebapp.controllers;
-
+ 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BasicController {
 
   @RequestMapping("/")
+  // @ResponseBody
   public String index() {
     return "hello.html";
   }
