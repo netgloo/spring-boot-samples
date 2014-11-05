cfd79ca97265f6810141162c8bca4a46ccde3f01
andreazanelli <andreazanelli5@gmail.com>
2014-11-05 18:18:15 +0100

    Removed unused files from springboot-basewebapp sample.

--- spring-boot-basewebapp/src/main/java/coderpills/Application.java
+++ spring-boot-basewebapp/src/main/java/coderpills/Application.java
@@ -1,4 +1,4 @@
-package basewebapp;
+package coderpills;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
--- spring-boot-basewebapp/src/main/java/coderpills/controllers/BasicController.java
+++ spring-boot-basewebapp/src/main/java/coderpills/controllers/BasicController.java
@@ -1,4 +1,4 @@
-package basewebapp.controllers;
+package coderpills.controllers;
  
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
