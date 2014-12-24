52806cf651b3ff4d1340d40b6629febc61cde2fb
azanelli <andreazanelli5@gmail.com>
2014-12-24 21:12:12 +0100

    Changed to non-static logger.

--- spring-boot-logging/src/main/java/netgloo/controllers/HomeController.java
+++ spring-boot-logging/src/main/java/netgloo/controllers/HomeController.java
@@ -10,20 +10,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
   // Define the log object for this class
-  private static final Logger _log =
-      LoggerFactory.getLogger(HomeController.class);
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
 
   @RequestMapping("/")
   @ResponseBody
   public String index() {
     
     // Log a simple message
-    _log.debug("debug level log");
-    _log.info("info level log");
-    _log.error("error level log");
+    log.debug("debug level log");
+    log.info("info level log");
+    log.error("error level log");
     
     // Log a formatted string with parameters
-    _log.info("another info log with {}, {} and {} arguments", 1, "2", 3.0);
+    log.info("another info log with {}, {} and {} arguments", 1, "2", 3.0);
     
     return "<a href='http://netgloo.com'>by netgloo</a>";
   }
