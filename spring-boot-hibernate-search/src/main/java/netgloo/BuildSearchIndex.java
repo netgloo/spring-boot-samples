d8c4fbce843fef6ee8752f903bfd70984d99a603
Andrea Zanelli <andrea.zanelli@netgloo.com>
2016-05-27 18:30:30 +0200

    Use ApplicationReadyEvent in BuildSearchIndex.

--- spring-boot-hibernate-search/src/main/java/netgloo/BuildSearchIndex.java
+++ spring-boot-hibernate-search/src/main/java/netgloo/BuildSearchIndex.java
@@ -5,8 +5,8 @@ import javax.persistence.PersistenceContext;
 
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.Search;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class BuildSearchIndex
-implements ApplicationListener<ContextRefreshedEvent> {
+implements ApplicationListener<ApplicationReadyEvent> {
   
   // ------------------------
   // PRIVATE FIELDS
@@ -42,10 +42,10 @@ implements ApplicationListener<ContextRefreshedEvent> {
    * refreshed.
    */
   @Override
-  public void onApplicationEvent(final ContextRefreshedEvent event) {
+  public void onApplicationEvent(final ApplicationReadyEvent event) {
     try {
       FullTextEntityManager fullTextEntityManager =
-          Search.getFullTextEntityManager(entityManager);
+        Search.getFullTextEntityManager(entityManager);
       fullTextEntityManager.createIndexer().startAndWait();
     }
     catch (InterruptedException e) {
